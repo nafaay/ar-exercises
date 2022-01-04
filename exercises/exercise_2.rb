170bc094bb202a9fdb443c50d1cc0b3db7ec95de
Youssef Nafaa <nafaayoussef@gmail.com>
2022-01-04 12:08:12 -0500

    update store exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.first
+puts @store1.name
+puts @store1.annual_revenue
+
+@store2 = Store.second
+puts @store2.name
+puts @store2.annual_revenue
+
+@store1.name = "Victoria"
+@store1.save
+
