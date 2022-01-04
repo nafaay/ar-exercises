16f18c140f123d9ded6bfc49f72a55e65df379bd
Youssef Nafaa <nafaayoussef@gmail.com>
2022-01-04 12:18:33 -0500

    delete store exercise_3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,13 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.third
+puts "Third store"
+puts @store3.name
+
+puts "Number of stores before deleting 3rd store"
+puts Store.all.count
+@store3.destroy
+puts "Number of stores after deleting 3rd store"
+puts Store.all.count
+
