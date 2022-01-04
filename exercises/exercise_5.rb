9b99ed4e6647de9dd9da9068db828317b0b8717b
Youssef Nafaa <nafaayoussef@gmail.com>
2022-01-04 13:31:51 -0500

    calculations exercise_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,22 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Sum All Revrnues"
+@sum = Store.sum(:annual_revenue)
+puts @sum
+
+@count = Store.all.count
+
+@average = @sum/@count
+puts "Average Annual Revenue"
+puts @average 
+
+puts "Stores generating more than 1M"
+@stores_more_than_1M = Store.where('annual_revenue > 1000000')
+@stores_more_than_1M.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+puts "Number Stores generating more than 1M"
+puts @stores_more_than_1M.all.count 
